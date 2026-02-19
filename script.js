82ea21c0adccc34ab8bf8f4739ccbf4dd0847123
matthieumarechal <tech@studiopepperoni.fr>
2026-02-19 12:26:43 +0100

    game logic working
    he current implementation allows the game to run for a single round, but it still needs optimization and improvements.

--- script.js
+++ script.js
@@ -3,6 +3,32 @@
 let machineChoice;
 let playerChoice;
 
+function whoWin (machineChoice, playerChoice) {
+
+    let result;
+    let comparison = `${machineChoice} ${playerChoice}`;
+
+    console.log("DEBBUG - comparison string : " + comparison);
+
+    if (machineChoice === playerChoice) {
+        console.log("DEBBUG - result is : Draw...");
+        return result = "Draw";
+    }
+    
+    result =
+        comparison === "rock paper" ? "You win!" :
+        comparison === "rock scissors" ? "You loose!" :
+
+        comparison === "paper rock" ? "You loose!" :
+        comparison === "paper scissors" ? "You win!" :
+
+        comparison === "scissors rock" ? "You win!" : "You loose!";
+
+        console.log("DEBBUG - result : " + result);
+
+        return;
+}
+
 // create 3 variables for the game : Rock Paper Scissors
 
 const rock = "rock";
@@ -32,7 +58,13 @@ playerChoice =
 
 console.log("DEBBUG - the player choose : " + playerChoice);
 
-// We pass our two player choice variables through the comparators and, based on that, we announce who wins the game.
+// We pass our two players choices variables through the comparators and, based on that, we announce who wins the game.
+
+whoWin(machineChoice, playerChoice);
+
+
+
+
 // We reset the variables Machine and Player
 // Ask to the player if he wants to play again ?
 // Play again or end the game
