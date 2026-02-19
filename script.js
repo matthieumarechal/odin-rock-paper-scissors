18649644f1fe7bbfb9d404f258c71359d5cc93bd
matthieumarechal <tech@studiopepperoni.fr>
2026-02-19 11:03:02 +0100

    Player slection
    Implement selection for the player's choice with a prompt and store his choice in a variable.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // create 2 variables : machine and player
 
-let machine;
-let player;
+let machineChoice;
+let playerChoice;
 
 // create 3 variables for the game : Rock Paper Scissors
 
@@ -12,14 +12,26 @@ const scissors = "scissors";
 // we randomly assign Rock Paper Scissors to the Machine
 
 let randomNumber = Math.floor(Math.random() * 3);
+console.log("DEBBUG - the random number for the machine is : " + randomNumber);
 
-machine = 
+machineChoice = 
     randomNumber === 0 ? rock :
     randomNumber === 1 ? paper : scissors;
 
-console.log("DEBBUG - the machine choose : " + machine);
+console.log("DEBBUG - the machine choose : " + machineChoice);
 
 // Ask for the player his choice about Rock Paper Scissors
+
+let choice = prompt("Make a choice ! Type the corresponding number of your choice : \n 1 - Rock\n 2 - Paper\n 3 - Scissors");
+
+playerChoice = 
+    choice === "" ? alert("you didn't type anything please try again") :
+    choice === "1" ? rock :
+    choice === "2" ? paper :
+    choice === "3" ? scissors : alert("You didn't type a good number");
+
+console.log("DEBBUG - the player choose : " + playerChoice);
+
 // We pass our two player choice variables through the comparators and, based on that, we announce who wins the game.
 // We reset the variables Machine and Player
 // Ask to the player if he wants to play again ?
