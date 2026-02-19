dd19b5bc6069d274b82e1ff7ad17727cf200bc7d
matthieumarechal <tech@studiopepperoni.fr>
2026-02-19 17:26:23 +0100

    Refactor and clean up game logic
    Optimize the code by removing an unnecessary function. Clean up development console logs. The game is now fully functional.

--- script.js
+++ script.js
@@ -14,24 +14,25 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
-// we randomly assign Rock Paper Scissors to the Machine
+// Function to get the Machine's choice with a math random and conditions
 
 function getMachineChoice() {
 
     let randomNumber = Math.floor(Math.random() * 3);
-    console.log("DEBBUG - the random number for the machine is : " + randomNumber);
+    //console.log("DEBBUG - the random number for the machine is : " + randomNumber);
 
     let choice = 
         randomNumber === 0 ? rock :
         randomNumber === 1 ? paper : scissors;
 
-    console.log("DEBBUG - the machine choose : " + choice);
+    //console.log("DEBBUG - the machine choose : " + choice);
 
     return choice;
 
 }
 
-// Ask for the player his choice with a number about Rock Paper Scissors
+// Function to get the player's choice with a prompt. I've decided to use a number as input in order to avoid case sensitive issues
+// Like the assignement said, we assume that player is going to give good inputs so we don't code anything in order to avoid player's errors...
 
 function getHumanChoice() {
 
@@ -43,23 +44,23 @@ function getHumanChoice() {
         number === "2" ? paper :
         number === "3" ? scissors : alert("You didn't type a good number");
 
-    console.log("DEBBUG - the player choose : " + choice);
+    //console.log("DEBBUG - the player choose : " + choice);
 
     return choice;
 
 }
 
-// We pass our two players choices variables through the comparators and, based on that, we announce who wins the game.
+// The function that plays a round, comparing machine and player strings and add it to the score
 
 function PlayRound(machineChoice, playerChoice) {
 
     let comparison = `${machineChoice} ${playerChoice}`;
 
-    console.log("DEBBUG - comparison string : " + comparison);
+    //console.log("DEBBUG - comparison string : " + comparison);
 
     if (machineChoice === playerChoice) {
         alert(`${machineChoice} and ${playerChoice} it's a draw...`)
-        console.log("DEBBUG - result is : Draw...");
+        //console.log("DEBBUG - result is : Draw...");
         return 
     }
     
@@ -74,28 +75,37 @@ function PlayRound(machineChoice, playerChoice) {
     return;
 }
 
+// function that runs the whole game for 5 rounds using a FOR
+
 function playGame() {
 
-    machineChoice = getMachineChoice();
-    playerChoice = getHumanChoice();
-    PlayRound(machineChoice, playerChoice);
-    console.log("DEBBUG - computer's score : " + computerScore + " human's score : " + humanScore);
+    for (i = 1; i < 6; i++) {
 
-}
+        console.log("- - - - - ROUND : " + i + " - - - - -");
 
+        machineChoice = getMachineChoice();
+        playerChoice = getHumanChoice();
 
-for (i = 1; i < 6; i++) {
-    
-    console.log("DEBBUG - round : " + i);
+        console.log("Computer choose : " + machineChoice);
+        console.log("You choose : " + playerChoice);
 
-    playGame();
-    
-    if (i === 5) {
-       let winner = (computerScore > humanScore) ? "Computer wins!" : "Human wins!";
-       console.log(winner);
+        PlayRound(machineChoice, playerChoice);
+        console.log("Computer's score : " + computerScore + " // human's score : " + humanScore);
+
+        if (i === 5) {
+            let winner = 
+                (computerScore > humanScore) ? "Computer wins!" : 
+                (computerScore < humanScore) ? "Human wins!" : "0 to 0, it's a draw..."
+            console.log(winner);
+        }
+
     }
-    
+
 }
+
+// call the main function
+playGame();
+
 
 
 
